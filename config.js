f3b9f3f4a3408afd8009451e76734705d67b5506
Najwanzaki <92720345+Najwanzaki@users.noreply.github.com>
2022-03-19 14:00:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['6285773744917', '6283872123464'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,10 +30,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'Zaky'
+global.author = 'NajwanZaky'
 
-global.wm = '© ariffb & melcanz'
+global.wm = '© Zaky'
 global.wait = '_*tunggu sedang diproses...*_'
 global.eror = '_*Server Error*_'
 global.benar = '✅'
