96c25a4baca791e094e8ca05433e054859dae1d4
Najwanzaki <92720345+Najwanzaki@users.noreply.github.com>
2022-03-20 15:05:55 +0000

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,13 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
 ┌「 *Donasi • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ https://saweria.co/Najwanzaky
+├ https://trakteer.id/najwanzaky
+├ 083872123464 (Axis)
+├ 083872123464 (GoPay) 
+├ 085773744917 (Dana)
+├ All Payment Tanpa Verifikasi Ktp PM Owner
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© Zaky', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
